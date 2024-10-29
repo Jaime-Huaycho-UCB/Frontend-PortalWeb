8fc95a6c5ba121de6916269b96d1420321fd56f3
Jaime Huaycho <jaimehuaycho@MacBook-Pro-de-Jaime.local>
2024-10-29 00:53:50 -0400

    camios de correcto funcionamiento

--- src/componentes/Encabezado.js
+++ src/componentes/Encabezado.js
@@ -7,7 +7,7 @@ import '../estilos/componentesEstilos/Encabezado.css';
 
 const Encabezado = () => {
   const { permiso } = useContext(AuthContext); 
-
+  
   return (
     <Navbar bg="light" expand="lg" className="encabezado">
       <Container>
@@ -15,7 +15,7 @@ const Encabezado = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {permiso === 1 ? (
+            {permiso == 1 ? (
               <>
                 <Nav.Link as={Link} to="/admin/gestion-docentes">Gestión Docentes</Nav.Link>
                 <Nav.Link as={Link} to="/admin/gestion-estudiantes">Gestión Estudiantes</Nav.Link>
@@ -23,7 +23,7 @@ const Encabezado = () => {
                 <Nav.Link as={Link} to="/admin/gestion-eventos">Gestión Eventos</Nav.Link>
                 <Nav.Link as={Link} to="/admin/crear-usuario">Crear Usuario Superior</Nav.Link>
               </>
-            ) : permiso === 0 ? (
+            ) : permiso == 0 ? (
               <>
                 <Nav.Link as={Link} to="/admin/gestion-estudiantes">Gestión Estudiantes</Nav.Link>
                 <Nav.Link as={Link} to="/admin/gestion-noticias">Gestión Noticias</Nav.Link>
